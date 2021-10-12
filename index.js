f158919f53f9fa24a6fa147d0cf1927e5039aba2
John Merz <jmerz826@gmail.com>
2021-10-12 17:10:26 -0400

    task 3 working, but feels clunky.

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function getFinals(data) {
     return result;
 }
 
-// console.log(getFinals(fifaData));
+console.log(getFinals(fifaData));
 
 
 
@@ -43,11 +43,16 @@ Use the higher-order function called getYears to do the following:
 2. Receive a callback function getFinals from task 2 
 3. Return an array called years containing all of the years in the getFinals data set*/
 
-function getYears(/* code here */) {
-    /* code here */
+function getYears(arr, getFinalsCb) {
+    const years = [];
+    const finalTeams = getFinalsCb(arr);
+
+    finalTeams.forEach(el => years.push(el['Year']));
+
+    return years;
 }
 
-
+console.log(getYears(fifaData, getFinals));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use the higher-order function getWinners to do the following:  
